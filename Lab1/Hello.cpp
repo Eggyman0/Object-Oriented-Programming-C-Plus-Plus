2a7250a3ea00732d8929272670cc0dbfeeb9f20b
eggyman <3ggyman@gmail.com>
2025-08-25 10:37:03 -0700

    created hello world demo

---
+++ Lab1/Hello.cpp
@@ -0,0 +1,12 @@
+/***
+ * Hello world demo
+***/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << endl;
+    printf("Hello World again!\n");
+    return 0;
+}
