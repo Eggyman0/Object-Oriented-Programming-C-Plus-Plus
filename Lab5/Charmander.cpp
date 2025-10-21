4282a086459cf98207000ca14aef4383d96245af
3ggyman0 <3ggyman@gmail.com>
2025-10-21 14:47:20 -0700

    Created Lab 5
    Added some code as well but still WIP

---
+++ Lab5/Charmander.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include "Pokemon.h"
+#include "Charmander.h"
+
+Charmander::Charmander(){
+    Pokemon Pokemon();
+    
+}
+
+Charmander::Charmander(string name, int hp, int att, int def, vector<string> t, vector<string> s){
+
+}
